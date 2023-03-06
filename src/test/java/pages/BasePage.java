687dba8fc80ce7d0730fd59a3b36b720a3c9384f
koberman758 <koberman758@gmail.com>
2023-03-06 13:07:26 +0300

    HW add CheckoutPage

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+abstract class BasePage {
+    WebDriver driver;
+    public static final String BASE_URL = "https://www.saucedemo.com/";
+    public static final By TITLE = By.cssSelector(".title");
+
+    BasePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public String getTitle() {
+        return driver.findElement(TITLE).getText();
+    }
+}
